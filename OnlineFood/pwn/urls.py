26ad93e52df5d1ff011a1074acae3d6ee244c1bb
mahaboobgithub <68046979+mahaboobgithub@users.noreply.github.com>
2020-09-07 09:17:07 +0530

    Revert "City,State,Cuisine modules updated"
    This reverts commit 7bfb46f1875d2757eaab5f2057a4ea8a9de438b0.

--- OnlineFood/pwn/urls.py
+++ OnlineFood/pwn/urls.py
@@ -1,7 +1,6 @@
-from django.conf.urls.static import static
+
 from django.urls import path
 
-from OnlineFood import settings
 from pwn import views
 
 urlpatterns = [
@@ -15,26 +14,4 @@ urlpatterns = [
     path('vendor/',views.openVendor,name='vendor'),
     path('resports/',views.openReporsts,name='reports'),
     path('logout/',views.pwn_login_check,name='logout'),
-    path('savestate/',views.savestate,name='savestate'),
-    path('updatestate/',views.updatestate,name='updatestate'),
-    path('updatestateid/',views.updatestateid,name='updatestateid'),
-    path('sdelete/',views.sdelete,name='sdelete'),
-
-    #city
-
-    path('savecity/',views.savecity,name='savecity'),
-    path('updatecity/',views.updatecity,name='updatecity'),
-    path('updatecityid/',views.updatecityid,name='updatecityid'),
-    path('cdelete/',views.cdelete,name='cdelete'),
-
-    #cuisine
-
-    path('savecuisine/',views.savecuisine,name='savecuisine'),
-    path('updatecuisine/',views.updatecuisine,name='updatecuisine'),
-    path('updatecuisineid/',views.updatecuisineid,name='updatecuisineid'),
-    path('dcuisine/',views.dcuisine,name='dcuisine')
-
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
